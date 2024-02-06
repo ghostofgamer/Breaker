2b220146172931847b4f1cb1739cabc94a1fdc6a
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2024-02-06 21:33:01 +0400

    AddNewInterface

--- Assets/Scripts/UI/Buttons/CloseShopButton.cs
+++ Assets/Scripts/UI/Buttons/CloseShopButton.cs
@@ -2,16 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CloseShopButton : AbstractButton
 {
     [SerializeField] private GameObject _screen;
     [SerializeField] private ShopBackGround _shopBackGround;
 
+    [SerializeField] private Image[] _imagesActive;
+    [SerializeField] private Image[] _backgroundImage;
+    [SerializeField] private Color _currentColor;
+    
     protected override void OnClick()
     {
         _screen.GetComponent<Animator>().Play("ShopScreenClose");
-        _shopBackGround.BackGroundAlphaChange(1,0);
+        _shopBackGround.BackGroundAlphaChange(1, 0);
         gameObject.SetActive(false);
+        OffImages();
+    }
+
+    private void OffImages()
+    {
+        foreach (Image image in _imagesActive)
+        {
+            image.gameObject.SetActive(false);
+        }
+
+        foreach (Image image in _backgroundImage)
+        {
+            image.color = _currentColor;
+        }
     }
 }
