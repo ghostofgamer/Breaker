e3059e0d1b3f41cd6dff74036d4f048c266efa8e
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2024-02-23 17:58:39 +0400

    AddPlatforma

--- Assets/Scenes/swipeMenu/swipe.cs
+++ Assets/Scenes/swipeMenu/swipe.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,14 +15,37 @@ public class swipe : MonoBehaviour
     private float time;
     private Button takeTheBtn;
     int btnNumber;
-    // Start is called before the first frame update
-    void Start()
+    private Vector3[] _startPosition;
+    private HorizontalLayoutGroup _layoutGroup;
+    [Range(0, 1000), SerializeField] private float _offset;
+    private RectTransform[] _children;
+    private Vector3[] _newPosition;
+    private void Start()
     {
+        _layoutGroup = GetComponent<HorizontalLayoutGroup>();
+        _startPosition = new Vector3[transform.childCount];
+        _children = new RectTransform[_layoutGroup.transform.childCount];
+        _newPosition = new Vector3[_children.Length];
 
+        for (int i = 0; i < _layoutGroup.transform.childCount; i++)
+        {
+            _children[i] = _layoutGroup.transform.GetChild(i).GetComponent<RectTransform>();
+        }
+
+        for (int i = 0; i < _children.Length; i++)
+        {
+            _newPosition[i] = _children[i].transform.localPosition;
+            _newPosition[i].z -= 50f;
+        }
+        
+        
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            _startPosition[i] = transform.GetChild(i).position;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         pos = new float[transform.childCount];
         float distance = 1f / (pos.Length - 1f);
@@ -62,12 +87,18 @@ public class swipe : MonoBehaviour
         {
             if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
             {
-                // Debug.LogWarning("Current Selected Level" + i);
-                transform.GetChild(i).localScale = Vector3.Lerp(transform.GetChild(i).localScale, new Vector3(1f, 1f,1f), 0.1f);
-                // transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector3(100f, 135f,100f), 0.1f);
+                Transform targetElement = _layoutGroup.transform.GetChild(i);
+                // targetElement.position = new Vector3( targetElement.position.x,  targetElement.position.y, targetElement.position.z - 1f);
+                transform.GetChild(i).localScale = Vector3.Lerp(transform.GetChild(i).localScale, new Vector3(1.3f, 1.3f,1.3f), 0.1f);
+                // transform.GetChild(i).localPosition = _newPosition[i];
+                // Vector3 targetposition = new Vector3(transform.GetChild(i).localPosition.x, transform.GetChild(i).localPosition.y, transform.GetChild(i).localPosition.z);
+                // Vector3 targetposition = new Vector3(transform.GetChild(i).position.x, transform.GetChild(i).position.y, -300f);
+                // transform.GetChild(i).position = Vector3.Lerp(transform.GetChild(i).position, targetposition, 0.1f);
+                // transform.GetChild(i).localPosition = Vector3.Lerp(transform.GetChild(i).position, targetposition, 0.1f);
+                transform.GetChild(i).localPosition = Vector3.Lerp(transform.GetChild(i).localPosition, new Vector3(transform.GetChild(i).localPosition.x,transform.GetChild(i).localPosition.y,-400f), 0.1f);
                 imageContent.transform.GetChild(i).localScale = Vector2.Lerp(imageContent.transform.GetChild(i).localScale, new Vector2(1.2f, 1.2f), 0.1f);
                 imageContent.transform.GetChild(i).GetComponent<Image>().color = colors[1];
-                
+                RotateCapsula(transform.GetChild(i),true);
                 for (int j = 0; j < pos.Length; j++)
                 {
                     if (j != i)
@@ -75,13 +106,29 @@ public class swipe : MonoBehaviour
                         imageContent.transform.GetChild(j).GetComponent<Image>().color = colors[0];
                         imageContent.transform.GetChild(j).localScale = Vector2.Lerp(imageContent.transform.GetChild(j).localScale, new Vector2(0.8f, 0.8f), 0.1f);
                         transform.GetChild(j).localScale = Vector3.Lerp(transform.GetChild(j).localScale, new Vector3(0.8f, 0.8f,0.8f), 0.1f);
-                        // transform.GetChild(j).localScale = Vector2.Lerp(transform.GetChild(j).localScale, new Vector3(80f, 100f,80f), 0.1f);
+                        transform.GetChild(j).localPosition = Vector3.Lerp(transform.GetChild(j).localPosition,new Vector3( transform.GetChild(j).localPosition.x, transform.GetChild(j).localPosition.y,-216f),0.1f);
+                        RotateCapsula(transform.GetChild(j), false);
+                        // transform.GetChild(j).localPosition = Vector3.Lerp(transform.GetChild(j).localPosition, new Vector3(transform.GetChild(j).localPosition.x,transform.GetChild(j).localPosition.y,-216f), 0.1f);
+                        // transform.GetChild(i).position = _startPosition[i];
+                        /*Vector3 targetPosition = new Vector3(transform.GetChild(i).position.x, transform.GetChild(i).position.y, -3.5f);
+                        transform.GetChild(i).position = Vector3.Lerp(transform.GetChild(i).position, targetPosition, 0.1f);*/
                     }
                 }
             }
         }
 
 
+    }
+
+    private void RotateCapsula(Transform capsula,bool selected)
+    {
+        var platforma = capsula.GetComponentInChildren<Capsula>();
+        platforma.StartRotate(selected);
+        // platforma.transform.rotation = Quaternion.Euler(0,0f,-45f);
+        /*platforma.transform.rotation = Quaternion.Euler(0,1f,0);
+        platforma.transform.Rotate(0,1,0);*/
+        /*capsula.transform.Rotate(0,1,0);
+        capsula.transform.localRotation *= Quaternion.Euler(0,100,0);*/
     }
 
     private void GecisiDuzenle(float distance, float[] pos, Button btn)
---
+++ Assets/Scripts/Capsula.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Capsula : MonoBehaviour
+{
+    private Quaternion originalRotation;
+    private Vector3  originalLocalRotation;
+    private bool _isSelected = false;
+    private Quaternion targetRotation;
+    private Quaternion _originalRotation;
+
+    private bool _rotate;
+    void Start()
+    {
+        originalRotation = transform.rotation;
+        // originalLocalRotation = transform.localRotation;
+        // originalLocalRotation = transform.localEulerAngles;
+        // targetRotation = originalRotation * Quaternion.Euler(45, 0, 0);
+    
+    }
+
+    void Update()
+    {
+        if (_isSelected)
+        {
+            if (!_rotate)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 45);
+                _rotate = true;
+            }
+            
+            Quaternion targetRotation = Quaternion.Euler(0, 0, 45);
+
+            /*if (transform.rotation != targetRotation)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * 30f);
+            }*/
+
+            transform.Rotate(0, 150 * Time.deltaTime, 0, Space.World);
+        }
+        else
+        {
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotation, Time.deltaTime * 5f);
+            _rotate = false;
+        }
+    }
+
+    public void StartRotate(bool selected)
+    {
+        _isSelected = selected;
+    }
+    
+    /*
+    private void Update()
+    {
+        // transform.rotation = Quaternion.Euler(0,0f,-45f);
+        transform.Rotate(0, 150 * Time.deltaTime, 0, Space.World);
+    }
+    */
+}
