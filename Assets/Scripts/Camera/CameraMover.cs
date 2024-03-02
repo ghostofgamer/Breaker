cb98bdc3c754db8427c528ac114b7d54d642bd76
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2024-03-02 12:01:20 +0400

    AddNewCameraMoveSystem

--- Assets/Scripts/Camera/CameraMover.cs
+++ Assets/Scripts/Camera/CameraMover.cs
@@ -1,87 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
-    /*
-    public float dragSpeed = 1f;
-    public float damping = 5f;
-    public float minX = -5f;
-    public float maxX = 13f;
-    public float minZ = -35f;
-    public float maxZ = 10f;
-    private Vector3 dragOrigin;
-    private Vector3 velocity = Vector3.zero;
-    private bool isDragging = false;
+    [SerializeField] private float _sensitivity;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _overSpeed;
+    [SerializeField] private float _minX;
+    [SerializeField] private float _maxX;
+    [SerializeField] private float _minZ;
+    [SerializeField] private float _maxZ;
+
+    private Vector3 _mouseStartPos;
+    private Vector3 _cameraStartPos;
+    private Vector3 _newCameraPos;
+
+    private void Start()
+    {
+        _mouseStartPos = Input.mousePosition;
+        _cameraStartPos = transform.position;
+        _newCameraPos = _cameraStartPos;
+    }
 
     void Update()
     {
+        if (transform.position != _newCameraPos)
+        {
+            Debug.Log(transform.position + "   ...   " + _newCameraPos);
+            transform.position = Vector3.MoveTowards(transform.position, _newCameraPos, _overSpeed * Time.deltaTime);
+        }
+
+
         if (Input.GetMouseButtonDown(0))
         {
-            dragOrigin = Input.mousePosition;
-            isDragging = true;
+            _mouseStartPos = Input.mousePosition;
+            _cameraStartPos = transform.position;
         }
-
-        if (Input.GetMouseButton(0))
+        else if (Input.GetMouseButton(0))
         {
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-            Vector3 move = new Vector3(-pos.x * dragSpeed, 0, -pos.y * dragSpeed);
-
-            // Применяем затухание к скорости перемещения только после отпускания кнопки мыши
-            if (isDragging)
-            {
-                velocity = move;
-            }
-            else
-            {
-                velocity = Vector3.Lerp(velocity, Vector3.zero, damping * Time.deltaTime);
-            }
-        }
-        else if (isDragging)
-        {
-            // Завершаем перетаскивание мыши
-            isDragging = false;
-        }
-
-        // Перемещаем камеру на основе текущей скорости
-        Vector3 newPosition = transform.position + velocity * Time.deltaTime;
-
-        // Проверяем, не выходит ли камера за пределы ограничений
-        float newX = Mathf.Clamp(newPosition.x, minX, maxX);
-        float newZ = Mathf.Clamp(newPosition.z, minZ, maxZ);
-
-        // Перемещаем камеру, если она не выходит за пределы ограничений
-        transform.position = new Vector3(newX, transform.position.y, newZ);
-    }
-    */
-    
-    
-    public float dragSpeed = 1;
-    private Vector3 dragOrigin;
-    public float minX = -3f;
-    public float maxX = 13f;
-    public float minZ = -30f;
-    public float maxZ = 10f;
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            dragOrigin = Input.mousePosition;
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-            Vector3 move = new Vector3(-pos.x * dragSpeed, 0, -pos.y * dragSpeed);
-            // Проверяем, не выходит ли камера за пределы ограничений
-            float newX = Mathf.Clamp(transform.position.x + move.x, minX, maxX);
-            float newZ = Mathf.Clamp(transform.position.z + move.z, minZ, maxZ);
-
-            // Перемещаем камеру, если она не выходит за пределы ограничений
-            transform.position = new Vector3(newX, transform.position.y, newZ);
-            // Debug.Log("CurentPosition " + transform.position);
+            Vector3 mouseDelta = Input.mousePosition - _mouseStartPos;
+            Vector3 cameraDelta = new Vector3(mouseDelta.x, 0, mouseDelta.y) * _sensitivity;
+            _newCameraPos = _cameraStartPos - cameraDelta;
+            _newCameraPos.x = Mathf.Clamp(_newCameraPos.x, _minX, _maxX);
+            _newCameraPos.z = Mathf.Clamp(_newCameraPos.z, _minZ, _maxZ);
+            transform.position = Vector3.Lerp(transform.position, _newCameraPos, _speed * Time.deltaTime);
         }
     }
+
+
+    //
+    // public float dragSpeed = 1;
+    // private Vector3 dragOrigin;
+    // public float minX = -3f;
+    // public float maxX = 13f;
+    // public float minZ = -30f;
+    // public float maxZ = 10f;
+    //
+    // void Update()
+    // {
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         dragOrigin = Input.mousePosition;
+    //     }
+    //
+    //     if (Input.GetMouseButton(0))
+    //     {
+    //         Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+    //         Vector3 move = new Vector3(-pos.x * dragSpeed, 0, -pos.y * dragSpeed);
+    //         // Проверяем, не выходит ли камера за пределы ограничений
+    //         float newX = Mathf.Clamp(transform.position.x + move.x, minX, maxX);
+    //         float newZ = Mathf.Clamp(transform.position.z + move.z, minZ, maxZ);
+    //
+    //         // Перемещаем камеру, если она не выходит за пределы ограничений
+    //         transform.position = new Vector3(newX, transform.position.y, newZ);
+    //         // Debug.Log("CurentPosition " + transform.position);
+    //     }
+    // }
 }
