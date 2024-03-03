6a6fbf1fe26339c2ac2bf9d6d6985ed1b540d0d0
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2024-03-03 19:45:11 +0400

    AddNewCuveFlyMainScene

--- Assets/Scripts/MainMenu/WaveMotion.cs
+++ Assets/Scripts/MainMenu/WaveMotion.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class WaveMotion : MonoBehaviour
 {
-    public GameObject cubePrefab; // Префаб для кубов
+    public GameObject cubePrefab;
     public float waveSpeed = 1.0f; // Скорость волны
     public float waveHeight = 1.0f; // Высота волны
     public float cubeSpacing = 1.5f; // Расстояние между кубами
@@ -19,73 +20,121 @@ public class WaveMotion : MonoBehaviour
     private List<GameObject> cubeList;
     public float minFlySpeed = 1.0f; // Минимальная скорость полета кубов
     public float maxFlySpeed = 3.0f;
+    
+    public int columns = 50;
+    public int rows = 50;
+
+    private GameObject[,] cubes;
+    private Vector3[,] startPositions;
+    private Vector3[,] endPositions;
+    private float[,] waveSpeeds;
+    private float time;
+
+    private bool _isFlyOver = false;
+
     void Start()
     {
         cubeList = new List<GameObject>();
-        // Инициализация массива для хранения ссылок на кубы
-        cubeGrid = new GameObject[gridSize, gridSize];
+        // cubeGrid = new GameObject[gridSize, gridSize];
+        cubeGrid = new GameObject[columns, rows];
 
-        // Запуск корутины для появления рядов кубов
-        StartCoroutine(SpawnCubeRows());
-    }
-
-    IEnumerator SpawnCubeRows()
-    {
-        // Создаем ряды кубов с задержкой
-        for (int i = 0; i < gridSize; i++)
+        /*for (int i = 0; i < gridSize; i++)
         {
             for (int j = 0; j < gridSize; j++)
             {
-                Vector3 position = startPosition + new Vector3(j * cubeSpacing, i * cubeSpacing, 0);
+                Vector3 position = startPosition - new Vector3(i * cubeSpacing, j * cubeSpacing, 0);
                 GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
                 cubeGrid[i, j] = cube;
                 cubeList.Add(cube);
             }
+        }*/
+        
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                Vector3 position = startPosition - new Vector3(i * cubeSpacing, j * cubeSpacing, 0);
+                GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
+                cubeGrid[i, j] = cube;
+                cubeList.Add(cube);
+            }
+        }
 
-            // Ждем заданное время перед созданием следующего ряда
+        StartCoroutine(MoveCubes());
+    }
+
+    IEnumerator MoveCubes()
+    {
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                Vector3 startPosition = cubeGrid[i, j].transform.position;
+                Vector3 endPosition = startPosition - new Vector3(0, 0, 15);
+                float speed = Random.Range(minFlySpeed, maxFlySpeed);
+                StartCoroutine(MoveCube(cubeGrid[i, j], endPosition, speed));
+            }
             yield return null;
         }
+
+        yield return new WaitForSeconds(0.3f);
+        _isFlyOver = true;
     }
+
+    IEnumerator MoveCube(GameObject cube, Vector3 target, float _speed)
+    {
+        while (cube.transform.position != target)
+        {
+            cube.transform.position = Vector3.MoveTowards(cube.transform.position, target, _speed * Time.deltaTime);
+            yield return null;
+        }
+
+
+        // StartCoroutine(Move());
+    }
+
 
     void Update()
     {
-        // Обновляем позиции кубов в сетке
-        for (int i = 0; i < gridSize; i++)
+        if (_isFlyOver)
         {
-            for (int j = 0; j < gridSize; j++)
+            for (int i = 0; i < columns; i++)
             {
-                if (cubeGrid[i, j] != null)
+                for (int j = 0; j < rows; j++)
                 {
-                    float waveOffset = Mathf.Sin((Time.time + (i + j)) * waveSpeed) * waveHeight;
-                    Vector3 newPosition = cubeGrid[i, j].transform.position + new Vector3(0, 0, waveOffset);
-                    cubeGrid[i, j].transform.position = newPosition;
+                    if (cubeGrid[i, j] != null)
+                    {
+                        float waveOffset = Mathf.Sin((Time.time + (i + j)) * waveSpeed) * waveHeight;
+                        Vector3 newPosition = cubeGrid[i, j].transform.position + new Vector3(0, 0, waveOffset);
+                        cubeGrid[i, j].transform.position = newPosition;
+                    }
                 }
             }
         }
 
-        /*if (Input.GetKeyDown(flyBackKey))
+
+        if (Input.GetKeyDown(flyBackKey))
         {
             FlyBackAllCubes();
             Debug.Log(cubeList.Count);
-        }*/
+        }
     }
-// Проверяем, была ли нажата клавиша для запуска кубов в обратном направлении
 
 
-   public void FlyBackAllCubes()
+    public void FlyBackAllCubes()
     {
         Debug.Log("FlyBackAllCubes");
-        // Запускаем все кубы в обратном направлении с разной скоростью
+        
         for (int i = 0; i < cubeList.Count; i++)
         {
             if (cubeList[i] != null)
             {
-                float flySpeed = UnityEngine.Random.Range(minFlySpeed, maxFlySpeed);
+                float flySpeed = UnityEngine.Random.Range(1, 5);
                 StartCoroutine(FlyBackCube(cubeList[i], flySpeed));
             }
         }
     }
-
+    
     IEnumerator FlyBackCube(GameObject cube, float speed)
     {
         Vector3 startPosition = cube.transform.position;
@@ -99,121 +148,8 @@ public class WaveMotion : MonoBehaviour
             elapsedTime += Time.deltaTime;
             yield return null;
         }
-
-        // Удаляем куб после полета
+        
         cubeList.Remove(cube);
-        Destroy(cube);
+        cube.gameObject.SetActive(false);
     }
-
-
-    /*public GameObject cubePrefab; // Префаб для кубов
-    public float waveSpeed = 1.0f; // Скорость волны
-    public float waveHeight = 1.0f; // Высота волны
-    public float cubeSpacing = 1.5f; // Расстояние между кубами
-    public int gridSize = 5; // Размер сетки (количество рядов и кубов в каждом ряду)
-
-    private GameObject[,] cubeGrid; // Массив для хранения ссылок на созданные кубы
-
-    void Start()
-    {
-        // Инициализация массива для хранения ссылок на кубы
-        cubeGrid = new GameObject[gridSize, gridSize];
-
-        // Создаем сетку из кубов
-        for (int i = 0; i < gridSize; i++)
-        {
-            for (int j = 0; j < gridSize; j++)
-            {
-                Vector3 position = new Vector3(i * cubeSpacing, j * cubeSpacing, 0);
-                GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
-                cubeGrid[i, j] = cube;
-            }
-        }
-    }
-
-    void Update()
-    {
-        // Обновляем позиции кубов в сетке
-        for (int i = 0; i < gridSize; i++)
-        {
-            for (int j = 0; j < gridSize; j++)
-            {
-                if (cubeGrid[i, j] != null)
-                {
-                    float waveOffset = Mathf.Sin((Time.time + (i + j)) * waveSpeed) * waveHeight;
-                    Vector3 newPosition = cubeGrid[i, j].transform.position + new Vector3(0, 0, waveOffset);
-                    cubeGrid[i, j].transform.position = newPosition;
-                }
-            }
-        }
-    }*/
-
-
-    /*
-    public GameObject cubePrefab; // Префаб для кубов
-    public float waveSpeed = 1.0f; // Скорость волны
-    public float waveHeight = 1.0f; // Высота волны
-    public float cubeSpacing = 1.5f; // Расстояние между кубами
-    public int squareSize = 5; // Размер квадрата (количество рядов и кубов в каждом ряду)
-    public int waveCount = 3; // Количество волн
-
-    private GameObject[,] cubeSquares; // Массив для хранения ссылок на созданные кубы
-
-    void Start()
-    {
-        // Инициализация массива для хранения ссылок на кубы
-        cubeSquares = new GameObject[squareSize, squareSize];
-
-        // Создаем квадрат из кубов
-        for (int i = 0; i < squareSize; i++)
-        {
-            for (int j = 0; j < squareSize; j++)
-            {
-                Vector3 position = new Vector3(i * cubeSpacing, 0, j * cubeSpacing);
-                GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
-                cubeSquares[i, j] = cube;
-            }
-        }
-    }
-
-    void Update()
-    {
-        // Обновляем позиции кубов в квадрате
-        for (int i = 0; i < squareSize; i++)
-        {
-            for (int j = 0; j < squareSize; j++)
-            {
-                if (cubeSquares[i, j] != null)
-                {
-                    float waveOffset = 0.0f;
-                    for (int k = 0; k < waveCount; k++)
-                    {
-                        float waveFrequency = (k + 1) * waveSpeed;
-                        float wavePhase = (i + j) * 0.1f * (k + 1);
-                        waveOffset += Mathf.Sin((Time.time + wavePhase) * waveFrequency) * waveHeight / (k + 1);
-                    }
-                    Vector3 newPosition = cubeSquares[i, j].transform.position + new Vector3(0, waveOffset, 0);
-                    cubeSquares[i, j].transform.position = newPosition;
-                }
-            }
-        }
-    }*/
-
-
-    /*[SerializeField] private float _speed = 1f;
-    [SerializeField] private float _amplitude = 1f;
-
-    private Vector3 _initialPosition;
-
-    private void Start()
-
-    {
-        _initialPosition = transform.position;
-        _amplitude = Random.Range(3f, 5f);
-    }
-    private void Update()
-    {
-        float z = Mathf.Sin(Time.time * _speed) * _amplitude;
-        transform.position = new Vector3(_initialPosition.x, _initialPosition.y, _initialPosition.z + z);
-    }*/
 }
