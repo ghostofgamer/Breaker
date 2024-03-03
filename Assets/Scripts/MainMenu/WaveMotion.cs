7d3607d45514e88cad1e2f90831253c873e95e61
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2024-03-03 19:58:24 +0400

    RefactoringScriptWave

--- Assets/Scripts/MainMenu/WaveMotion.cs
+++ Assets/Scripts/MainMenu/WaveMotion.cs
@@ -6,57 +6,38 @@ using Random = UnityEngine.Random;
 
 public class WaveMotion : MonoBehaviour
 {
-    public GameObject cubePrefab;
-    public float waveSpeed = 1.0f; // Скорость волны
-    public float waveHeight = 1.0f; // Высота волны
-    public float cubeSpacing = 1.5f; // Расстояние между кубами
-    public int gridSize = 5; // Размер сетки (количество рядов и кубов в каждом ряду)
-    public float spawnDelay = 1.0f; // Задержка между появлением рядов кубов
-    public Vector3 startPosition; // Начальная позиция для появления сетки кубов
+    [SerializeField] private GameObject _cubePrefab;
+    [SerializeField] private float _waveSpeed;
+    [SerializeField] private float _waveHeight;
+    [SerializeField] private float _cubeSpacing;
+    [SerializeField] private Vector3 _startPosition;
+    [SerializeField] private KeyCode _flyBackKey = KeyCode.Space;
+    [SerializeField] private float _minFlySpeed;
+    [SerializeField] private float _maxFlySpeed;
+    [SerializeField] private int _columns;
+    [SerializeField] private int _rows;
 
-    private GameObject[,] cubeGrid; // Массив для хранения ссылок на созданные кубы
-    public KeyCode flyBackKey = KeyCode.Space;
-
-    private List<GameObject> cubeList;
-    public float minFlySpeed = 1.0f; // Минимальная скорость полета кубов
-    public float maxFlySpeed = 3.0f;
-    
-    public int columns = 50;
-    public int rows = 50;
-
-    private GameObject[,] cubes;
-    private Vector3[,] startPositions;
-    private Vector3[,] endPositions;
-    private float[,] waveSpeeds;
-    private float time;
-
+    private GameObject[,] _cubeGrid;
+    private List<GameObject> _cubeList;
+    private GameObject[,] _cubes;
+    private Vector3[,] _startPositions;
+    private Vector3[,] _endPositions;
+    private float _time;
     private bool _isFlyOver = false;
 
     void Start()
     {
-        cubeList = new List<GameObject>();
-        // cubeGrid = new GameObject[gridSize, gridSize];
-        cubeGrid = new GameObject[columns, rows];
+        _cubeList = new List<GameObject>();
+        _cubeGrid = new GameObject[_columns, _rows];
 
-        /*for (int i = 0; i < gridSize; i++)
+        for (int i = 0; i < _columns; i++)
         {
-            for (int j = 0; j < gridSize; j++)
+            for (int j = 0; j < _rows; j++)
             {
-                Vector3 position = startPosition - new Vector3(i * cubeSpacing, j * cubeSpacing, 0);
-                GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
-                cubeGrid[i, j] = cube;
-                cubeList.Add(cube);
-            }
-        }*/
-        
-        for (int i = 0; i < columns; i++)
-        {
-            for (int j = 0; j < rows; j++)
-            {
-                Vector3 position = startPosition - new Vector3(i * cubeSpacing, j * cubeSpacing, 0);
-                GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
-                cubeGrid[i, j] = cube;
-                cubeList.Add(cube);
+                Vector3 position = _startPosition - new Vector3(i * _cubeSpacing, j * _cubeSpacing, 0);
+                GameObject cube = Instantiate(_cubePrefab, position, Quaternion.identity);
+                _cubeGrid[i, j] = cube;
+                _cubeList.Add(cube);
             }
         }
 
@@ -65,15 +46,16 @@ public class WaveMotion : MonoBehaviour
 
     IEnumerator MoveCubes()
     {
-        for (int i = 0; i < columns; i++)
+        for (int i = 0; i < _columns; i++)
         {
-            for (int j = 0; j < rows; j++)
+            for (int j = 0; j < _rows; j++)
             {
-                Vector3 startPosition = cubeGrid[i, j].transform.position;
+                Vector3 startPosition = _cubeGrid[i, j].transform.position;
                 Vector3 endPosition = startPosition - new Vector3(0, 0, 15);
-                float speed = Random.Range(minFlySpeed, maxFlySpeed);
-                StartCoroutine(MoveCube(cubeGrid[i, j], endPosition, speed));
+                float speed = Random.Range(_minFlySpeed, _maxFlySpeed);
+                StartCoroutine(MoveCube(_cubeGrid[i, j], endPosition, speed));
             }
+
             yield return null;
         }
 
@@ -81,65 +63,57 @@ public class WaveMotion : MonoBehaviour
         _isFlyOver = true;
     }
 
-    IEnumerator MoveCube(GameObject cube, Vector3 target, float _speed)
+    IEnumerator MoveCube(GameObject cube, Vector3 target, float speed)
     {
         while (cube.transform.position != target)
         {
-            cube.transform.position = Vector3.MoveTowards(cube.transform.position, target, _speed * Time.deltaTime);
+            cube.transform.position = Vector3.MoveTowards(cube.transform.position, target, speed * Time.deltaTime);
             yield return null;
         }
-
-
-        // StartCoroutine(Move());
     }
-
 
     void Update()
     {
         if (_isFlyOver)
         {
-            for (int i = 0; i < columns; i++)
+            for (int i = 0; i < _columns; i++)
             {
-                for (int j = 0; j < rows; j++)
+                for (int j = 0; j < _rows; j++)
                 {
-                    if (cubeGrid[i, j] != null)
+                    if (_cubeGrid[i, j] != null)
                     {
-                        float waveOffset = Mathf.Sin((Time.time + (i + j)) * waveSpeed) * waveHeight;
-                        Vector3 newPosition = cubeGrid[i, j].transform.position + new Vector3(0, 0, waveOffset);
-                        cubeGrid[i, j].transform.position = newPosition;
+                        float waveOffset = Mathf.Sin((Time.time + (i + j)) * _waveSpeed) * _waveHeight;
+                        Vector3 newPosition = _cubeGrid[i, j].transform.position + new Vector3(0, 0, waveOffset);
+                        _cubeGrid[i, j].transform.position = newPosition;
                     }
                 }
             }
         }
 
-
-        if (Input.GetKeyDown(flyBackKey))
+        if (Input.GetKeyDown(_flyBackKey))
         {
             FlyBackAllCubes();
-            Debug.Log(cubeList.Count);
+            Debug.Log(_cubeList.Count);
         }
     }
-
 
     public void FlyBackAllCubes()
     {
-        Debug.Log("FlyBackAllCubes");
-        
-        for (int i = 0; i < cubeList.Count; i++)
+        foreach (GameObject cube in _cubeList)
         {
-            if (cubeList[i] != null)
+            if (cube != null)
             {
                 float flySpeed = UnityEngine.Random.Range(1, 5);
-                StartCoroutine(FlyBackCube(cubeList[i], flySpeed));
+                StartCoroutine(FlyBackCube(cube, flySpeed));
             }
         }
     }
-    
+
     IEnumerator FlyBackCube(GameObject cube, float speed)
     {
         Vector3 startPosition = cube.transform.position;
-        Vector3 endPosition = startPosition - new Vector3(0, 0, -10); // Кубы полетают назад на 10 единиц по оси Z
-        float flyTime = 1.0f / speed; // Время полета куба в зависимости от его скорости
+        Vector3 endPosition = startPosition - new Vector3(0, 0, -10);
+        float flyTime = 1.0f / speed;
         float elapsedTime = 0;
 
         while (elapsedTime < flyTime)
@@ -148,8 +122,8 @@ public class WaveMotion : MonoBehaviour
             elapsedTime += Time.deltaTime;
             yield return null;
         }
-        
-        cubeList.Remove(cube);
+
+        _cubeList.Remove(cube);
         cube.gameObject.SetActive(false);
     }
 }
