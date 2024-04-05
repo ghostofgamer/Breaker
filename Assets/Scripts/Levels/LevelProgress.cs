2583d7ab630d7b06d894daf4f6b3507e7134439a
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2024-04-05 15:49:29 +0400

    StartRepairBricks

--- Assets/Scripts/Levels/LevelProgress.cs
+++ Assets/Scripts/Levels/LevelProgress.cs
@@ -5,9 +5,6 @@ namespace Levels
 {
     public class LevelProgress : Level
     {
-        // [SerializeField] private Level[] _nextLevel;
-        // [SerializeField] private EffectChanger _effectChanger;
-
         private LevelState _currentLevelState;
 
         public void SetLevels()
@@ -20,18 +17,15 @@ namespace Levels
 
                     if (State == LevelState.Completed && _currentLevelState == LevelState.Completed)
                     {
-                        Debug.Log("1");
                         EffectChanger.SetLine(i, PassedColor);
                         EffectChanger.LineMoveActivation(i);
                     }
                     else if (State >= LevelState.Unlocked && _currentLevelState >= LevelState.Unlocked)
                     {
-                        Debug.Log("136");
                         EffectChanger.SetLine(i, State == LevelState.Completed ? PassedColor : NotPassedColor);
                     }
                     else
                     {
-                        Debug.Log("15");
                         EffectChanger.SetLine(i, NotOpenColor);
                     }
                 }
